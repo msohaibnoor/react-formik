0e107e9d3b117e8a0337b75da100eb31b20f43fc
DESKTOP-ENARB9E\Sohaib Noor <sohaib.noor@aquila360.com>
2022-04-12 17:00:18 +0500

    first commit

---
+++ src/components/YoutubeForm.js
@@ -0,0 +1,47 @@
+import { useFormik } from 'formik';
+import React from 'react';
+
+const YoutubeForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      channel: '',
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+  //   console.log(formik.values);
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="name">Name</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        onChange={formik.handleChange}
+        value={formik.values.name}
+      />
+      <label htmlFor="email">E-mail</label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+      <label htmlFor="channel">Channel</label>
+      <input
+        type="text"
+        id="channel"
+        name="channel"
+        onChange={formik.handleChange}
+        value={formik.values.channel}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default YoutubeForm;
